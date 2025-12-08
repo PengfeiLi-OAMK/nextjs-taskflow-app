6b3e857cd218663367c45c3fb21a894916520e3d
PengfeiLi-OAMK <t3lipe01@students.oamk.fi>
2025-12-08 11:24:49 +0200

    unsplash integration + FormPicker component

--- components/form/FormPopover.tsx
+++ components/form/FormPopover.tsx
@@ -9,10 +9,13 @@ import { useAction } from '@/hooks/use-action';
 import { Button } from '@/components/ui/button';
 import { createBoard } from '@/actions/create-board';
 
-import { FormInput} from './FormInput';
+import { FormInput } from './FormInput';
 import { FormSubmit } from './FormSubmit';
 import { X } from 'lucide-react';
 import { toast } from 'sonner';
+import FormPicker from './FormPicker';
+import { ElementRef, useRef } from 'react';
+import { useRouter } from 'next/navigation';
 interface FormPopoverProps {
   children: React.ReactNode;
   side?: 'left' | 'right' | 'top' | 'bottom';
@@ -20,63 +23,63 @@ interface FormPopoverProps {
   sideOffset?: number;
 }
 
-export const FormPopover=({
-	children,
-	side='bottom',
-	align,
-	sideOffset=0,
-}:FormPopoverProps)=>{
-	const { execute, fieldErrors } = useAction(createBoard, {
-      onSuccess(data) {
-        console.log({data});
-		toast.success('Board created successfully!');
-      },
-      onError(error) {
-        console.log({error});
-		toast.error(error);
-      },
-    });
-	const onSubmit = (formData: FormData) => {
+export const FormPopover = ({
+  children,
+  side = 'bottom',
+  align,
+  sideOffset = 0,
+}: FormPopoverProps) => {
+  const closeRef = useRef<ElementRef<'button'>>(null);
+  const router = useRouter();
+  const { execute, fieldErrors } = useAction(createBoard, {
+    onSuccess(data) {
+      toast.success('Board created successfully!');
+      closeRef.current?.click();
+      router.push(`/board/${data.id}`);
+    },
+    onError(error) {
+      console.log({ error });
+      toast.error(error);
+    },
+  });
+  const onSubmit = (formData: FormData) => {
     const title = formData.get('title') as string;
-    execute({ title });
+    const image = formData.get('image') as string;
+    execute({ title, image });
   };
-	return(
-		<Popover>
-			  <PopoverTrigger asChild>
-				{children}
-			  </PopoverTrigger>
-			  <PopoverContent
-			  	align={align}
-				className='w-80 pt-3'
-				side={side}
-				sideOffset={sideOffset}
-			  >
-				<div className='text-sm font-semibold text-center text-neutral-600 pb-4'>
-					Create New Board
-				</div>
-				<PopoverClose asChild>
-					<Button
-						className='h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600'
-						variant='ghost'
-					>
-						<X className='h-4 w-4'/>
-					</Button>
-				</PopoverClose>
-				<form action={onSubmit} className='space-y-4'>
-					<div className='space-y-4'>
-						<FormInput
-							id='title'
-							label='Board Title'
-							type='text'
-							errors={fieldErrors}
-							/>
-					</div>
-					<FormSubmit className='w-full'>
-						Create
-					</FormSubmit>
-				</form>
-			  </PopoverContent>
-		</Popover>
-	)
-
-}
+  return (
+    <Popover>
+      <PopoverTrigger asChild>{children}</PopoverTrigger>
+      <PopoverContent
+        align={align}
+        className="w-80 pt-3"
+        side={side}
+        sideOffset={sideOffset}
+      >
+        <div className="text-sm font-semibold text-center text-neutral-600 pb-4">
+          Create New Board
+        </div>
+        <PopoverClose ref={closeRef} asChild>
+          <Button
+            className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600"
+            variant="ghost"
+          >
+            <X className="h-4 w-4" />
+          </Button>
+        </PopoverClose>
+        <form action={onSubmit} className="space-y-4">
+          <div className="space-y-4">
+            <FormPicker id="image" errors={fieldErrors} />
+            <FormInput
+              id="title"
+              label="Board Title"
+              type="text"
+              errors={fieldErrors}
+            />
+          </div>
+          <FormSubmit className="w-full">Create</FormSubmit>
+        </form>
+      </PopoverContent>
+    </Popover>
+  );
+};
--- next.config.js
+++ next.config.js
@@ -13,6 +13,10 @@ const nextConfig = {
 
         hostname: 'img.clerk.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
     ],
   },
 };
