18ca2dd4b7e5c8c8256e8405709ea2e16d27ae58
PengfeiLi-OAMK <t3lipe01@students.oamk.fi>
2025-12-18 22:37:18 +0200

    card modal

--- lib/create-safe-action.ts
+++ lib/create-safe-action.ts
@@ -11,7 +11,7 @@ export const createSafeAction = <TInput, TOutput>(
   schema: z.Schema<TInput>,
   handler: (validateData: TInput) => Promise<ActionState<TInput, TOutput>>
 ) => {
-	return async(data:TInput): Promise<ActionState<TInput, TOutput>>=>{
+	return async (data:TInput): Promise<ActionState<TInput, TOutput>>=>{
 		const validateResult=schema.safeParse(data);
 		if(!validateResult.success){
 			return{
